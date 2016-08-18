a856b17a7f332dce056ea32128221d7eabfaab64
Liu Cui <c.liu@piala.co.jp>
2016-08-18 18:36:21 +0900

    init
    使用webpack构建 Angular2应用 的基础配置级一个简单的例子

---
+++ config/webpack.common.js
@@ -0,0 +1,69 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    // 在大多数现代浏览器中运行 Angular 2 程序时需要的标准填充物。
+    // 早点加载 Zone.js ，紧跟在其它 ES6 和 metadata 垫片 (shim) 之后。
+    'polyfills' : './src/polyfills.ts', 
+
+    // 需要的提供商文件： Angular 2 、 Lodash 、 bootstrap.css 等
+    'vendor'    : './src/vendor.ts', 
+
+    // 应用代码
+    'app'       : './src/main.ts' 
+  },
+
+  
+
+  resolve: {
+    // 一个明确的扩展名 ( 通过一个空白的扩展名字符串 '' 标记出来 )
+    extensions: ['', '.js', '.ts']
+  },
+
+  
+
+  module: {
+    //指定加载器
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['ts', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        exclude: helpers.root('src', 'app'),
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+      },
+      {
+        test: /\.css$/,
+        include: helpers.root('src', 'app'),
+        loader: 'raw'
+      }
+    ]
+  },
+
+  
+
+  plugins: [
+    //这里标记出了三个 块儿 之间的等级体系： app -> vendor -> polyfills 
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    //自动注入 打包后的JS和CSS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,29 @@
+/*
+ * 开发环境配置文件
+ */
+
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+//通过 webpackMerge 可以覆盖common里的配置
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: 'http://localhost:8080/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    stats: 'minimal'
+  }
+});
